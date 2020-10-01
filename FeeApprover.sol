afaffccb40ca79098bb34fe7ab423233b6e0d464
1AndOnlyPika <1andonlypika@gmail.com>
2020-09-30 19:35:31 -0700

    v1
    EnCore Vault v1

---
+++ FeeApprover.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+import "@openzeppelin/contracts-ethereum-package/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol"; // for WETH
+import "@nomiclabs/buidler/console.sol";
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
+import "@openzeppelin/contracts-ethereum-package/contracts/Initializable.sol";
+
+contract FeeApprover is OwnableUpgradeSafe {
+    using SafeMath for uint256;
+
+    function initialize(
+        address _ENCOREAddress,
+        address _WETHAddress,
+        address _uniswapFactory
+    ) public initializer {
+        OwnableUpgradeSafe.__Ownable_init();
+        encoreTokenAddress = _ENCOREAddress;
+        WETHAddress = _WETHAddress;
+        tokenUniswapPair = IUniswapV2Factory(_uniswapFactory).getPair(WETHAddress,encoreTokenAddress);
+        feePercentX100 = 10;
+        paused = true; // We start paused until sync post LGE happens.
+    }
+
+    address tokenUniswapPair;
+    IUniswapV2Factory public uniswapFactory;
+    address internal WETHAddress;
+    address encoreTokenAddress;
+    address encoreVaultAddress;
+    uint8 public feePercentX100;  // max 255 = 25.5% artificial clamp
+    uint256 public lastTotalSupplyOfLPTokens;
+    bool paused;
+
+    // Pausing transfers of the token
+    function setPaused(bool _pause) public onlyOwner {
+        paused = _pause;
+    }
+
+    function setFeeMultiplier(uint8 _feeMultiplier) public onlyOwner {
+        feePercentX100 = _feeMultiplier;
+    }
+
+    function setEncoreVaultAddress(address _encoreVaultAddress) public onlyOwner {
+        encoreVaultAddress = _encoreVaultAddress;
+    }
+
+    function sync() public {
+        uint256 _LPSupplyOfPairTotal = IERC20(tokenUniswapPair).totalSupply();
+        lastTotalSupplyOfLPTokens = _LPSupplyOfPairTotal;
+    }
+
+    function calculateAmountsAfterFee(
+        address sender,
+        address recipient, // unusued maybe use din future
+        uint256 amount
+        ) public  returns (uint256 transferToAmount, uint256 transferToFeeDistributorAmount)
+        {
+            require(paused == false, "FEE APPROVER: Transfers Paused");
+            uint256 _LPSupplyOfPairTotal = IERC20(tokenUniswapPair).totalSupply();
+
+
+            // console.log("sender is " , sender);
+            // console.log("recipient is is " , recipient, 'pair is :', tokenUniswapPair);
+
+            // console.log("Old LP supply", lastTotalSupplyOfLPTokens);
+            // console.log("Current LP supply", _LPSupplyOfPairTotal);
+
+            if(sender == tokenUniswapPair)
+                require(lastTotalSupplyOfLPTokens <= _LPSupplyOfPairTotal, "Liquidity withdrawals forbidden");
+
+            // console.log('Sender is pair' , sender == tokenUniswapPair);
+            // console.log('lastTotalSupplyOfLPTokens <= _LPSupplyOfPairTotal' , lastTotalSupplyOfLPTokens <= _LPSupplyOfPairTotal);
+
+            if(sender == encoreVaultAddress  || sender == tokenUniswapPair ) { // Dont have a fee when encorevault is sending, or infinite loop
+                console.log("Sending without fee");                       // And when pair is sending ( buys are happening, no tax on it)
+                transferToFeeDistributorAmount = 0;
+                transferToAmount = amount;
+            }
+            else {
+                console.log("Normal fee transfer");
+                transferToFeeDistributorAmount = amount.mul(feePercentX100).div(1000);
+                transferToAmount = amount.sub(transferToFeeDistributorAmount);
+            }
+
+
+           lastTotalSupplyOfLPTokens = _LPSupplyOfPairTotal;
+        }
+
+
+}
---
+++ IEncoreVault.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.6.0;
+
+
+interface IEncoreVault {
+    function addPendingRewards(uint _amount) external;
+}
